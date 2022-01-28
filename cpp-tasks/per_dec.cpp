bea569e262735a35b491c27d78fb452b8e7faa45
elys33ve <elys33ve@gmail.com>
2022-01-28 03:44:29 -0500

    per_dec.cpp

---
+++ cpp-tasks/per_dec.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   //variable
+   double percentage;
+   double d;
+   //input
+   cout << "integer percent:  ";    //(without percent sign)
+   cin >> percentage;
+   //divide
+   d = percentage / 100;
+   //output
+   cout << d << endl;
+
+   return 0;
+}
