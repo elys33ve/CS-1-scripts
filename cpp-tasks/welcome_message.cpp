70449bf4f3d6b70c1253be920d0649338a2795b0
elys33ve <elys33ve@gmail.com>
2022-01-28 23:20:52 -0500

    welcome_message.cpp

---
+++ cpp-tasks/welcome_message.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+//takes 1st name as input and outputs:
+//"Hello *name*, and welcome to CS Online!"
+
+int main() {
+//variable
+   string nam;
+
+//input 
+   cin >> nam;
+
+//output
+   cout << "Hello " << nam << ", and welcome to CS Online!\n";
+
+   return 0;
+}
