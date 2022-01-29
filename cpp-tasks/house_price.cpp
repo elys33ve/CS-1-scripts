f60a05fb36d70c54b7731ed0b0e47c868df6dd1a
elys33ve <elys33ve@gmail.com>
2022-01-28 21:09:01 -0500

    house_price.cpp

--- cpp-tasks/house_price.cpp
+++ cpp-tasks/house_price.cpp
@@ -2,17 +2,18 @@
 using namespace std;
 
 int main() {
+//variables
    int cp;
    int lp;
    int em;
    int ch;
-   
+//inputs
    cin >> cp;
    cin >> lp;
-
+//maths
    em = (cp * 0.051)/12;
    ch = cp - lp;
-
+//outputs
    cout << "This house is $" << cp << ". The change is $" << ch << " since last month.\n";
    cout << "The estimated monthly mortgage is $" << em << ".\n";
 
